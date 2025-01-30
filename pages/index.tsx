4a9c96010df211bd6736ec405b1c3d7e15e531f3
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-01-30 11:28:39 -0300

    fix: beauthfy for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import styles from "./index.module.css";
 
 const App = () => {
   return (
-    <div className={styles.footer}>
-      <img src="../soon.gif" />
+    <div className={styles.container}>
+      <Image alt="coming soon" fill={true} className="image" src="/soon.gif" />
     </div>
   );
 };
