da5f629633f9f0c1d160bed33127c1f73fad4bb8
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-02-11 20:24:27 -0300

    chore: adding education section

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,14 @@ export default function App() {
           </p>
         </div>
       </div>
+      <div className="education">
+        <h1>Formação</h1>
+        <div>
+          <p>2015 - 2019</p>
+          <p>Universidade de Franca</p>
+          <p>Bacharelado em Ciência da Computação</p>
+        </div>
+      </div>
       <Loading />
       <footer></footer>
     </div>
