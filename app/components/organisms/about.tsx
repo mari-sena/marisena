ed85e5d56bbd4eba598c50c359b63ecd5c7ab039
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-02-11 20:35:33 -0300

    forgotten class at globals.css

---
+++ app/components/organisms/about.tsx
@@ -0,0 +1,37 @@
+import "./about.css";
+
+import Image from "next/image";
+
+function About() {
+  return (
+    <div className="about-container">
+      <div className="about-wrapper">
+        <div className="about-image">
+          <Image
+            alt="coming soon"
+            width={259}
+            height={327}
+            className="image"
+            src="/about-me.png"
+          />
+        </div>
+        <div>
+          <h1>Mariana Sena</h1>
+          <p>Desenvolvedora de Software</p>
+        </div>
+      </div>
+      <div className="about-me-description">
+        <h4>Olá!</h4>
+        <p>
+          Sou desenvolvedora de software, tenho facilidade em colaborar em
+          equipe e estou sempre buscando eficiência e boas práticas de
+          segurança! Apaixonada por cachororros, estou sempre na companhia de um
+          livro, adoro filmes e séries e estou sempre disposta a aprender coisas
+          novas!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,13 @@
 import "./globals.css";
 
-import Image from "next/image";
-
+import About from "./components/organisms/about";
 import Loading from "./loading";
 import Education from "./components/organisms/education";
 
 export default function App() {
   return (
     <div className="container">
-      <div className="about-container">
-        <div className="about-wrapper">
-          <div className="about-image">
-            <Image
-              alt="coming soon"
-              width={259}
-              height={327}
-              className="image"
-              src="/about-me.png"
-            />
-          </div>
-          <div>
-            <h1>Mariana Sena</h1>
-            <p>Desenvolvedora de Software</p>
-          </div>
-        </div>
-        <div className="about-me-description">
-          <h4>Olá!</h4>
-          <p>
-            Sou desenvolvedora de software, tenho facilidade em colaborar em
-            equipe e estou sempre buscando eficiência e boas práticas de
-            segurança! Apaixonada por cachororros, estou sempre na companhia de
-            um livro, adoro filmes e séries e estou sempre disposta a aprender
-            coisas novas!
-          </p>
-        </div>
-      </div>
+      <About />
       <Education />
       <Loading />
       <footer></footer>
