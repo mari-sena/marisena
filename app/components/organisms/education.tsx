dfce6177854259081111e0d35ff6c94060081aa8
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-02-11 20:31:58 -0300

    refactor: education into a component

---
+++ app/components/organisms/education.tsx
@@ -0,0 +1,14 @@
+import "./education.css";
+
+export default function Education() {
+  return (
+    <div className="education">
+      <h1>Formação</h1>
+      <div>
+        <p>2015 - 2019</p>
+        <p>Universidade de Franca</p>
+        <p>Bacharelado em Ciência da Computação</p>
+      </div>
+    </div>
+  );
+}
