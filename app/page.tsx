e595bda3339d598343c90c1fef42fb796b8d0df4
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-10 09:06:18 -0300

    loading effect and organizing home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import "./globals.css";
 
 import About from "./components/organisms/about";
-import Loading from "./loading";
 import Education from "./components/organisms/education";
+import WorkExperience from "./components/organisms/work-experience";
 
 export default function App() {
   return (
-    <div className="container">
+    <div className="container loading-screen">
+      <div className="work-experience">
+        <WorkExperience />
+        <Education />
+      </div>
       <About />
-      <Education />
-      <Loading />
       <footer></footer>
     </div>
   );
