76dec6f692434c929f41365a4ae26adb14b6dad9
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-10 13:06:02 -0300

    chore: improving globals style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 export const metadata = {
-  title: "Sena's portfólio",
+  title: "Mariana Sena's portfólio",
   description: "Bem-vindo ao meu portfólio!",
+  keywords: [
+    "Mariana Sena",
+    "Software Developer",
+    "Desenvolvedora de Software",
+    "Next.js",
+    "React",
+    "JavaScript",
+  ],
+  languages: {
+    "pt-BR": "/pt-BR",
+    "en-US": "/en-US",
+  },
   icons: {
     icon: "/computador-portatil.png",
   },
