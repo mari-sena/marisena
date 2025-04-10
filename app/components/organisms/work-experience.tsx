30d5fa8a9813a2d5396f01a7bafa91d217403005
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-10 09:05:58 -0300

    work experience update

---
+++ app/components/organisms/work-experience.tsx
@@ -0,0 +1,62 @@
+import "./about.css";
+
+function WorkExperience() {
+  return (
+    <>
+      <h1>Experience</h1>
+      <>
+        <h2>Software Developer</h2>
+        <h3>IBM 12/2021 - 09/2024</h3>
+        <p>
+          Desenvolvi e corrigi funcionalidades front-end utilizando React.js,
+          Vue.js, TypeScript, SASS e Tailwind CSS, criando interfaces modernas e
+          responsivas, além de resolver bugs em aplicações front-end e back-end
+          com Next.js e Nest.js.
+          <br />
+          <br />
+          Analisei tickets de suporte, identificando problemas no fornecimento
+          de dados, explorando APIs e código back-end para solucionar
+          causas-raiz, executando queries em MongoDB e scripts em servidores
+          para processar dados de fornecedores.
+          <br />
+          <br />
+          Realizei análises de segurança com Contrast Security em projetos
+          front-end e back-end, identificando e remediando vulnerabilidades,
+          além de mitigar riscos por meio de correções estratégicas e boas
+          práticas de segurança.
+          <br />
+          <br />
+          Implementei pipelines de CI/CD para automatizar processos de build,
+          testes e deploy, aumentando a eficiência e confiabilidade nas
+          entregas.
+          <br />
+        </p>
+      </>
+      <>
+        <br />
+        <h2>Front-End Developer</h2>
+        <h3>FPASS 03/2020 - 11/2021</h3>
+        <p>
+          Desenvolvi interfaces para uma plataforma educacional utilizando
+          principalmente React.js e SCSS, integrei os dados da API da plataforma
+          com Typescript e liderei uma equipe de 2 desenvolvedores, onde eu
+          gerenciava tarefas e contribuia ativamente para o desenvolvimento da
+          aplicação.
+          <br />
+        </p>
+      </>
+      <>
+        <br />
+        <h2>Front-End Developer</h2>
+        <h3>CloudMed 10/2018 - 06/2019</h3>
+        <p>
+          Conduzi o desenvolvimento da landing page da empresa. Minha função
+          envolvia criar o design das telas, desenvolve-las utilizando React.js,
+          estiliza-las com CSS e garantia a sua responsividade.
+        </p>
+      </>
+    </>
+  );
+}
+
+export default WorkExperience;
