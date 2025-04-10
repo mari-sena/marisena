a9a75ce2859c7a8473d67c1d208c7675d93f3e8a
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-10 09:05:43 -0300

    education update

--- app/components/organisms/education.tsx
+++ app/components/organisms/education.tsx
@@ -3,11 +3,22 @@ import "./education.css";
 export default function Education() {
   return (
     <div className="education">
+      <br />
       <h1>Formação</h1>
       <div>
-        <p>2015 - 2019</p>
-        <p>Universidade de Franca</p>
+        <h3>Universidade de Franca 2015 - 2019</h3>
         <p>Bacharelado em Ciência da Computação</p>
+        <strong>
+          Projeto de Graduação - Plataforma de Prontuários Médicos
+        </strong>
+        <p>Período: 07/2019 - 12/2019</p>
+        <p>
+          Desenvolvi uma plataforma web que centraliza e exibe o prontuário
+          eletrônico de pacientes para médicos, com o objetivo de facilitar o
+          acesso às informações e reduzir erros. Implementei o front-end
+          utilizando React.js e CSS; Estruturei o back-end com Node.js e banco
+          de dados MongoDB;
+        </p>
       </div>
     </div>
   );
