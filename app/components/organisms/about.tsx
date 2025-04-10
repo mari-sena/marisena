a5dc3299e4a98fb2a35a7a53c42d00d415adf641
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-10 09:04:43 -0300

    about me section update

--- app/components/organisms/about.tsx
+++ app/components/organisms/about.tsx
@@ -6,6 +6,16 @@ function About() {
   return (
     <div className="about-container">
       <div className="about-wrapper">
+        <div className="about-me-description">
+          <h4>Olá!</h4>
+          <p>
+            Sou desenvolvedora de software, tenho facilidade em colaborar em
+            equipe e frequêntemente em busca de aprimorar minhas habilidades e
+            em aumentar a eficiência e as boas práticas de segurança! Apaixonada
+            por cachorros, estou sempre na companhia de um livro, adoro filmes e
+            séries e estou sempre disposta a aprender coisas novas!
+          </p>
+        </div>
         <div className="about-image">
           <Image
             alt="coming soon"
@@ -19,16 +29,6 @@ function About() {
           <h1>Mariana Sena</h1>
           <p>Desenvolvedora de Software</p>
         </div>
-      </div>
-      <div className="about-me-description">
-        <h4>Olá!</h4>
-        <p>
-          Sou desenvolvedora de software, tenho facilidade em colaborar em
-          equipe e estou sempre buscando eficiência e boas práticas de
-          segurança! Apaixonada por cachorros, estou sempre na companhia de um
-          livro, adoro filmes e séries e estou sempre disposta a aprender coisas
-          novas!
-        </p>
       </div>
     </div>
   );
