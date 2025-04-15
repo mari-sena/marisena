71626d11a9a6ef6ffa3a8b89694bbbea2fb49dcb
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-15 14:08:40 -0300

    isto n esta funcionandoo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,18 @@
 import "./not-found.css";
 import Link from "next/link";
-import useTranslation from "next-translate/useTranslation";
 
 export default function NotFound() {
-  const { t } = useTranslation("not-found");
-
   return (
     <div className="content">
       <Link href="/" className="return-link">
-        {t("title")}
+        {"title"}
       </Link>
       <div className="title">
         <span className="floating-letter delay0">4</span>
         <span className="floating-letter delay200">0</span>
         <span className="floating-letter delay400">4</span>
       </div>
-      <div className="subtitle">{t("message")}</div>
+      <div className="subtitle">{"message"}</div>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,1 @@
-const nextTranslate = require("next-translate-plugin");
-
-module.exports = nextTranslate({
-  reactStrictMode: true,
-});
+module.exports = {};
