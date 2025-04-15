a47964f19954120111d67c40f86a9470d6212e92
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-04-15 14:30:08 -0300

    fix: not found without translation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,14 @@ export default function NotFound() {
   return (
     <div className="content">
       <Link href="/" className="return-link">
-        {"title"}
+        Retornar
       </Link>
       <div className="title">
         <span className="floating-letter delay0">4</span>
         <span className="floating-letter delay200">0</span>
         <span className="floating-letter delay400">4</span>
       </div>
-      <div className="subtitle">{"message"}</div>
+      <div className="subtitle">Página não encontrada</div>
     </div>
   );
 }
